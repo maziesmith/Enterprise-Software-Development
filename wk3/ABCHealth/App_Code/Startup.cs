906b8d87c6b7dced777e566350395271d0bc55cb
thomaslwk <154433k@mymail.nyp.edu.sg>
2017-11-03 20:36:00 +0800

    Wk 3 cassignment 50% complete

---
+++ wk3/ABCHealth/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ABCHealth.Startup))]
+namespace ABCHealth
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
