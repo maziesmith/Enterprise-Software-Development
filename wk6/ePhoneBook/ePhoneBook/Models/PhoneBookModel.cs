3c856390623eca25e2168596f5b89a20decc2c34
thomaslwk <154433k@mymail.nyp.edu.sg>
2017-11-22 09:33:38 +0800

    Import project for wk 6 practical

---
+++ wk6/ePhoneBook/ePhoneBook/Models/PhoneBookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ePhoneBook.Models
+{
+    public class PhoneBookModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage ="The {0} must be at least {2} characters long.", MinimumLength =6)]
+
+        public String FirstName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        public String MiddleName { get; set; }
+    }
+}
