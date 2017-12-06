96966311a683e1076d0d8c3570687b8a4ad84c16
thomaslwk <154433k@mymail.nyp.edu.sg>
2017-12-06 09:54:30 +0800

    Import project

---
+++ wk8/Practical_7/Practical_7/Services/TemperatureConverter.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="TemperatureConverter.asmx.cs" Class="Practical_7.Services.TemperatureConverter" %>
---
+++ wk8/Practical_7/Practical_7/Services/TemperatureConverter.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Practical_7.Services
+{
+    /// <summary>
+    /// Summary description for TemperatureConverter
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class TemperatureConverter : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        //public string HelloWorld()
+        //{
+        //    return "Hello World";
+        //}
+        public double ToCelsius(double temperature)
+        {
+            return Math.Round(9 / 5 * (temperature - 32), 1);
+        }
+        [WebMethod]
+        public double ToFahrenheit(double temperature)
+        {
+            return Math.Round(9 / 5 * temperature + 32, 1);
+        }
+    }
+}
